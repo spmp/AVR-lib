3b9b0454fd5f3790366ee7f7bfa1a0a8e7450d6c
Jasper Aorangi <jasper.mackenzie@gmail.com>
2014-07-23 17:46:36 +1200

    Lots of changes to an effectively broken pwm.c

--- lib/pid.c
+++ lib/pid.c
@@ -48,6 +48,24 @@ void pid_Init(int16_t p_factor, int16_t i_factor, int16_t d_factor, struct PID_D
   pid->maxSumError = MAX_I_TERM / (pid->I_Factor + 1);
 }
 
+/**
+ * @brief same as above but with uint16_t
+ **/
+void u_pid_Init(int16_t p_factor, int16_t i_factor, int16_t d_factor, struct u_PID_DATA *pid)
+// Set up PID controller parameters
+{
+  // Start values for PID controller
+  pid->sumError = 0;
+  pid->lastProcessValue = 0;
+  // Tuning constants for PID loop
+  pid->P_Factor = p_factor;
+  pid->I_Factor = i_factor;
+  pid->D_Factor = d_factor;
+  // Limits to avoid overflow
+  pid->maxError = MAX_LONG / (pid->P_Factor + 1);
+  pid->maxSumError = MAX_I_TERM / (pid->I_Factor + 1);
+}
+
 
 /**
  * @brief PID control algorithm.
@@ -97,12 +115,72 @@ int16_t pid_Controller(int16_t setPoint, int16_t processValue, struct PID_DATA *
   pid_st->lastProcessValue = processValue;
 
   ret = (p_term + i_term + d_term) / SCALING_FACTOR;
-  if(ret > MAX_INT){
-    ret = MAX_INT;
+  if(ret > MAX_UINT){
+    ret = MAX_UINT;
   }
-  else if(ret < -MAX_INT){
+  else if(ret < -MAX_UINT){
     ret = -MAX_INT;
   }
+
+  return((int16_t)ret);
+}
+
+/**
+ * @brief PID control algorithm.
+ *
+ * Calculates output from setpoint, process value and PID status.
+ *
+ * @param setPoint  Desired value.
+ * @param processValue  Measured value.
+ * @param pid_st  PID status struct.
+ **/
+int16_t u_pid_Controller(uint16_t setPoint, uint16_t processValue, struct u_PID_DATA *pid_st)
+{
+    //JA Increase to int32 to contain the maxiumum swing of uint16_t. i.e error must be able to handle +-2^16, p_term must be able to handle +-16*some factor.
+    // P_factor can only be as great as 2^15 ie int16
+  int32_t error, p_term=0, d_term=0;
+  int32_t i_term, ret, temp;
+
+  error = setPoint - processValue;
+
+  // Calculate Pterm and limit error overflow
+  if (error > pid_st->maxError){
+    p_term = MAX_UINT;
+  }
+  else if (error < -pid_st->maxError){
+    p_term = -MAX_UINT;
+  }
+  else{
+    p_term = pid_st->P_Factor * error;
+  }
+
+  // Calculate Iterm and limit integral runaway
+  temp = pid_st->sumError + error;
+  if(temp > pid_st->maxSumError){
+    i_term = MAX_I_TERM;
+    pid_st->sumError = pid_st->maxSumError;
+  }
+  else if(temp < -pid_st->maxSumError){
+    i_term = -MAX_I_TERM;
+    pid_st->sumError = -pid_st->maxSumError;
+  }
+  else{
+    pid_st->sumError = temp;
+    i_term = pid_st->I_Factor * pid_st->sumError;
+  }
+
+  // Calculate Dterm
+  d_term = pid_st->D_Factor * (pid_st->lastProcessValue - processValue);
+
+  pid_st->lastProcessValue = processValue;
+
+  ret = (p_term + i_term + d_term) / SCALING_FACTOR;
+//   if(ret > MAX_INT){
+//     ret = MAX_INT;
+//   }
+//   else if(ret < -MAX_INT){
+//     ret = -MAX_INT;
+//   }
 
   return((int16_t)ret);
 }
--- lib/pid.h
+++ lib/pid.h
@@ -46,13 +46,36 @@ typedef struct PID_DATA{
   int32_t maxSumError;
 } pidData_t;
 
+/*! \brief PID Status
+ *
+ * Setpoints and data used by the PID control algorithm
+ */
+typedef struct u_PID_DATA{
+  //! Last process value, used to find derivative of process value.
+  uint16_t lastProcessValue;
+  //! Summation of errors, used for integrate calculations
+  int32_t sumError;
+  //! The Proportional tuning constant, multiplied with SCALING_FACTOR
+  int16_t P_Factor;
+  //! The Integral tuning constant, multiplied with SCALING_FACTOR
+  int16_t I_Factor;
+  //! The Derivative tuning constant, multiplied with SCALING_FACTOR
+  int16_t D_Factor;
+  //! Maximum allowed error, avoid overflow
+  int32_t maxError;
+  //! Maximum allowed sumerror, avoid overflow
+  int32_t maxSumError;
+} u_pidData_t;
+
 /*! \brief Maximum values
  *
  * Needed to avoid sign/overflow problems
  */
 // Maximum value of variables
 #define MAX_INT         0x7FFF
+#define MAX_UINT        0xFFFF
 #define MAX_LONG        0x7FFFFFFFL
+#define MAX_ULONG       0xFFFFFFFFL
 #define MAX_I_TERM      (0x7FFFFFFFL / 2)
 
 // Boolean values
@@ -70,6 +93,11 @@ typedef struct PID_DATA{
 void pid_Init(int16_t p_factor, int16_t i_factor, int16_t d_factor, struct PID_DATA *pid);
 
 /**
+ * @brief same as above but with uint16_t
+ **/
+void u_pid_Init(int16_t p_factor, int16_t i_factor, int16_t d_factor, struct u_PID_DATA *pid);
+
+/**
  * @brief PID control algorithm.
  *
  *  Calculates output from setpoint, process value and PID status.
@@ -80,6 +108,11 @@ void pid_Init(int16_t p_factor, int16_t i_factor, int16_t d_factor, struct PID_D
  * @return Output update, int16
  */
 int16_t pid_Controller(int16_t setPoint, int16_t processValue, struct PID_DATA *pid_st);
+
+/**
+ * @brief Same as above but for uint16_t
+ **/
+int16_t u_pid_Controller(uint16_t setPoint, uint16_t processValue, struct u_PID_DATA *pid_st);
 
 /**
  * @brief Resets the integrator.
